989470b36a07b8505761552b9a1c9e2e5642543b
Michael Stone <aucompbiker@gmail.com>
2019-04-02 13:05:11 -0500

    Prevent the user from logging into the system if access to the item is restricted to everyone.

--- dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/RestrictedItem.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/RestrictedItem.java
@@ -7,10 +7,14 @@
  */
 package org.dspace.app.xmlui.aspect.artifactbrowser;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.cocoon.environment.ObjectModelHelper;
 import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.http.HttpEnvironment;
+import org.apache.cocoon.ResourceNotFoundException;
+import org.apache.commons.lang.StringUtils;
+
 import org.apache.log4j.Logger;
+
 import org.dspace.app.xmlui.cocoon.AbstractDSpaceTransformer;
 import org.dspace.app.xmlui.utils.AuthenticationUtil;
 import org.dspace.app.xmlui.utils.HandleUtil;
@@ -25,98 +29,76 @@ import org.dspace.content.*;
 import org.dspace.content.Item;
 import org.dspace.content.factory.ContentServiceFactory;
 import org.dspace.content.service.BitstreamService;
-import org.dspace.embargo.AUETDEmbargoSetter;
-import org.dspace.embargo.factory.EmbargoServiceFactory;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.UUID;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.cocoon.ResourceNotFoundException;
-import org.apache.cocoon.environment.http.HttpEnvironment;
+
 /**
  * Display an item restricted message.
  *
  * @author Scott Phillips
- * @author Mark Diggory  mdiggory at atmire dot com
+ * @author Mark Diggory mdiggory at atmire dot com
  * @author Fabio Bolognisi fabio at atmire dot com
  */
-public class RestrictedItem extends AbstractDSpaceTransformer //implements CacheableProcessingComponent
+public class RestrictedItem extends AbstractDSpaceTransformer // implements CacheableProcessingComponent
 {
     private static final Logger log = Logger.getLogger(RestrictedItem.class);
 
     /**
      * language strings
      */
-    private static final Message T_title =
-            message("xmlui.ArtifactBrowser.RestrictedItem.title");
+    private static final Message T_title = message("xmlui.ArtifactBrowser.RestrictedItem.title");
 
-    private static final Message T_dspace_home =
-            message("xmlui.general.dspace_home");
+    private static final Message T_dspace_home = message("xmlui.general.dspace_home");
 
-    private static final Message T_trail =
-            message("xmlui.ArtifactBrowser.RestrictedItem.trail");
+    private static final Message T_trail = message("xmlui.ArtifactBrowser.RestrictedItem.trail");
 
-    private static final Message T_head_resource =
-            message("xmlui.ArtifactBrowser.RestrictedItem.head_resource");
+    private static final Message T_head_resource = message("xmlui.ArtifactBrowser.RestrictedItem.head_resource");
 
-    private static final Message T_head_community =
-            message("xmlui.ArtifactBrowser.RestrictedItem.head_community");
+    private static final Message T_head_community = message("xmlui.ArtifactBrowser.RestrictedItem.head_community");
 
-    private static final Message T_head_collection =
-            message("xmlui.ArtifactBrowser.RestrictedItem.head_collection");
+    private static final Message T_head_collection = message("xmlui.ArtifactBrowser.RestrictedItem.head_collection");
 
-    private static final Message T_head_item =
-            message("xmlui.ArtifactBrowser.RestrictedItem.head_item");
-    
-    private static final Message T_head_item_withdrawn =
-            message("xmlui.ArtifactBrowser.RestrictedItem.head_item_withdrawn");
+    private static final Message T_head_item = message("xmlui.ArtifactBrowser.RestrictedItem.head_item");
 
-    private static final Message T_head_bitstream =
-            message("xmlui.ArtifactBrowser.RestrictedItem.head_bitstream");
+    private static final Message T_head_item_withdrawn = message(
+            "xmlui.ArtifactBrowser.RestrictedItem.head_item_withdrawn");
 
-    private static final Message T_para_resource =
-            message("xmlui.ArtifactBrowser.RestrictedItem.para_resource");
+    private static final Message T_head_bitstream = message("xmlui.ArtifactBrowser.RestrictedItem.head_bitstream");
 
-    private static final Message T_para_community =
-            message("xmlui.ArtifactBrowser.RestrictedItem.para_community");
+    private static final Message T_para_resource = message("xmlui.ArtifactBrowser.RestrictedItem.para_resource");
 
-    private static final Message T_para_collection =
-            message("xmlui.ArtifactBrowser.RestrictedItem.para_collection");
+    private static final Message T_para_community = message("xmlui.ArtifactBrowser.RestrictedItem.para_community");
 
-    private static final Message T_para_item =
-            message("xmlui.ArtifactBrowser.RestrictedItem.para_item");
-    
-    private static final Message T_para_bitstream =
-            message("xmlui.ArtifactBrowser.RestrictedItem.para_bitstream");
+    private static final Message T_para_collection = message("xmlui.ArtifactBrowser.RestrictedItem.para_collection");
 
+    private static final Message T_para_item = message("xmlui.ArtifactBrowser.RestrictedItem.para_item");
+
+    private static final Message T_para_bitstream = message("xmlui.ArtifactBrowser.RestrictedItem.para_bitstream");
 
     // Item states
-    private static final Message T_para_item_restricted_auth =
-            message("xmlui.ArtifactBrowser.RestrictedItem.para_item_restricted_auth");
-    private static final Message T_para_item_restricted =
-            message("xmlui.ArtifactBrowser.RestrictedItem.para_item_restricted");
-    private static final Message T_para_item_withdrawn =
-            message("xmlui.ArtifactBrowser.RestrictedItem.para_item_withdrawn");
+    private static final Message T_para_item_restricted_auth = message(
+            "xmlui.ArtifactBrowser.RestrictedItem.para_item_restricted_auth");
+    private static final Message T_para_item_restricted = message(
+            "xmlui.ArtifactBrowser.RestrictedItem.para_item_restricted");
+    private static final Message T_para_item_withdrawn = message(
+            "xmlui.ArtifactBrowser.RestrictedItem.para_item_withdrawn");
 
-
-    private static final Message T_para_login =
-            message("xmlui.ArtifactBrowser.RestrictedItem.login");
+    private static final Message T_para_login = message("xmlui.ArtifactBrowser.RestrictedItem.login");
 
     protected BitstreamService bitstreamService = ContentServiceFactory.getInstance().getBitstreamService();
 
     protected AuthorizeService authorizeService = AuthorizeServiceFactory.getInstance().getAuthorizeService();
 
-
-    public void addPageMeta(PageMeta pageMeta) throws SAXException,
-            WingException, UIException, SQLException, IOException,
-            AuthorizeException 
-    {
+    public void addPageMeta(PageMeta pageMeta)
+            throws SAXException, WingException, UIException, SQLException, IOException, AuthorizeException {
         DSpaceObject dso = HandleUtil.obtainHandle(objectModel);
         Request request = ObjectModelHelper.getRequest(objectModel);
         Item item = null;
-		
+
         pageMeta.addMetadata("title").addContent(T_title);
 
         pageMeta.addTrailLink(contextPath + "/", T_dspace_home);
@@ -125,15 +107,12 @@ public class RestrictedItem extends AbstractDSpaceTransformer //implements Cache
         }
         pageMeta.addTrail().addContent(T_trail);
 
-        if(StringUtils.isNotBlank(request.getParameter("bitstreamId"))) {
-			Bitstream bitstream = bitstreamService.find(context, UUID.fromString(request.getParameter("bitstreamId")));
-        
-            if(authorizeService.isAccessRestrictedToAllNonAdminUsers(context, bitstream))
-            {
+        if (StringUtils.isNotBlank(request.getParameter("bitstreamId"))) {
+            Bitstream bitstream = bitstreamService.find(context, UUID.fromString(request.getParameter("bitstreamId")));
+
+            if (authorizeService.isAccessRestrictedToAllNonAdminUsers(context, bitstream)) {
                 pageMeta.addMetadata("view-account-nav").addContent("false");
-            }
-            else
-            {
+            } else {
                 pageMeta.addMetadata("view-account-nav").addContent("true");
             }
         } else {
@@ -141,44 +120,36 @@ public class RestrictedItem extends AbstractDSpaceTransformer //implements Cache
         }
     }
 
-
-    public void addBody(Body body) throws SAXException, WingException,
-        UIException, SQLException, IOException, AuthorizeException, 
-        ResourceNotFoundException 
-    {
+    public void addBody(Body body) throws SAXException, WingException, UIException, SQLException, IOException,
+            AuthorizeException, ResourceNotFoundException {
         Request request = ObjectModelHelper.getRequest(objectModel);
         DSpaceObject dso = HandleUtil.obtainHandle(objectModel);
 
         Division unauthorized = null;
         boolean isWithdrawn = false;
-        
-        if (dso == null) 
-        {
+
+        if (dso == null) {
             unauthorized = body.addDivision("unauthorized-resource", "primary");
             unauthorized.setHead(T_head_resource);
             unauthorized.addPara(T_para_resource);
-        } 
-        else if (dso instanceof Community) 
-        {
+        } else if (dso instanceof Community) {
             Community community = (Community) dso;
             unauthorized = body.addDivision("unauthorized-resource", "primary");
             unauthorized.setHead(T_head_community);
             unauthorized.addPara(T_para_community.parameterize(community.getName()));
-        } 
-        else if (dso instanceof Collection) 
-        {
+        } else if (dso instanceof Collection) {
             Collection collection = (Collection) dso;
             unauthorized = body.addDivision("unauthorized-resource", "primary");
             unauthorized.setHead(T_head_collection);
             unauthorized.addPara(T_para_collection.parameterize(collection.getName()));
-        } 
-        else if (dso instanceof Item) 
-        {
-            // The dso may be an item but it could still be an item's bitstream. So let's check for the parameter.
+        } else if (dso instanceof Item) {
+            // The dso may be an item but it could still be an item's bitstream. So let's
+            // check for the parameter.
             if (StringUtils.isNotBlank(request.getParameter("bitstreamId"))) {
                 String identifier = "unknown";
                 try {
-                    Bitstream bit = bitstreamService.find(context, UUID.fromString(request.getParameter("bitstreamId")));                    
+                    Bitstream bit = bitstreamService.find(context,
+                            UUID.fromString(request.getParameter("bitstreamId")));
                     if (bit != null) {
                         identifier = bit.getName();
                     }
@@ -204,16 +175,14 @@ public class RestrictedItem extends AbstractDSpaceTransformer //implements Cache
                 String divID = "restricted";
                 Message title = T_head_item;
                 Message status = T_para_item_restricted;
-                //if item is withdrawn, display withdrawn status info
-                if (((Item) dso).isWithdrawn()) 
-                {
+                // if item is withdrawn, display withdrawn status info
+                if (((Item) dso).isWithdrawn()) {
                     divID = "withdrawn";
                     title = T_head_item_withdrawn;
                     status = T_para_item_withdrawn;
                     isWithdrawn = true;
-                }//if user is not authenticated, display info to authenticate
-                else if (context.getCurrentUser() == null) 
-                {
+                } // if user is not authenticated, display info to authenticate
+                else if (context.getCurrentUser() == null) {
                     status = T_para_item_restricted_auth;
                 }
                 unauthorized = body.addDivision(divID, "primary");
@@ -221,19 +190,19 @@ public class RestrictedItem extends AbstractDSpaceTransformer //implements Cache
                 unauthorized.addPara(T_para_item.parameterize(identifier));
                 unauthorized.addPara("item_status", status.getKey()).addContent(status);
             }
-        } // end if Item 
-        else 
-        {
-            // This case should not occur, but if it does just fall back to the resource message.
+        } // end if Item
+        else {
+            // This case should not occur, but if it does just fall back to the resource
+            // message.
             unauthorized = body.addDivision("unauthorized-resource", "primary");
             unauthorized.setHead(T_head_resource);
             unauthorized.addPara(T_para_resource);
         }
 
-        // Interrupt the request if the current user is not loggedIn, and the item is not withdrawn
-        if (!isWithdrawn && context.getCurrentUser() == null) 
-        {
-            //unauthorized.addPara().addXref(contextPath+"/login", T_para_login);
+        // Interrupt the request if the current user is not loggedIn, and the item is
+        // not withdrawn
+        if (!isWithdrawn && context.getCurrentUser() == null) {
+            // unauthorized.addPara().addXref(contextPath+"/login", T_para_login);
 
             // Interrupt request if the user is not authenticated, so they may come back to
             // the restricted resource afterwards.
@@ -244,10 +213,10 @@ public class RestrictedItem extends AbstractDSpaceTransformer //implements Cache
             // Interrupt this request
             AuthenticationUtil.interruptRequest(objectModel, header, message, characters);
         }
-        
-        //Finally, set proper response. Return "404 Not Found" for all restricted/withdrawn items
-        HttpServletResponse response = (HttpServletResponse)objectModel
-		.get(HttpEnvironment.HTTP_RESPONSE_OBJECT);   
+
+        // Finally, set proper response. Return "404 Not Found" for all
+        // restricted/withdrawn items
+        HttpServletResponse response = (HttpServletResponse) objectModel.get(HttpEnvironment.HTTP_RESPONSE_OBJECT);
         response.setStatus(HttpServletResponse.SC_NOT_FOUND);
     }
 }
