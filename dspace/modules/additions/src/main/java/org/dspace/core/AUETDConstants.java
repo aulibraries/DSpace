10a3b4aad21ba201a055ab2763a87e116945d2f2
Michael Stone <aucompbiker@gmail.com>
2020-04-29 09:03:21 -0500

    Initial commit

---
+++ dspace/modules/additions/src/main/java/org/dspace/core/AUETDConstants.java
@@ -0,0 +1,17 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+
+package org.dspace.core;
+
+public class AUETDConstants {
+
+    public static final String EMBARGOED = "EMBARGOED";
+    public static final String NOT_EMBARGOED = "NOT_EMBARGOED";
+    public static final String EMBARGO_NOT_AUBURN_STR = "EMBARGO_NOT_AUBURN";
+    public static final String EMBARGO_GLOBAL_STR = "EMBARGO_GLOBAL";
+}
